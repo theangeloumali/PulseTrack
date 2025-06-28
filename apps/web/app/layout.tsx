1b2ad0cb949f57d4fbfb5e7bc1e2da70b42f4911
GeloDGreat <carions46@gmail.com>
2025-06-28 12:29:27 +0800

    feat: integrate Vercel Analytics for enhanced tracking and performance insights

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,30 +1,29 @@
-import { Geist, Geist_Mono } from "next/font/google"
-
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
+import '@workspace/ui/globals.css';
+import { Providers } from '@/components/providers';
 
 const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+	subsets: ['latin'],
+	variable: '--font-mono',
+});
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
+				<Analytics />
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
