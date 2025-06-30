c28afa8c48a467ea749c289cd6bd5208ceeff924
GeloDGreat <carions46@gmail.com>
2025-06-30 09:58:45 +0800

    base config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  basePath: "/pulse",
+  assetPrefix: "/pulse",
 }
 
 export default nextConfig
